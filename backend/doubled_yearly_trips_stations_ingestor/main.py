c52a9a619e60eb068ac39f00fba5a3b58c445617
andrezszambrano <andreskilla2000@gmail.com>
2023-06-17 15:30:55 -0300

    Main for doubled_yearly_trips_ingestor set

--- backend/doubled_yearly_trips_stations_ingestor/main.py
+++ backend/doubled_yearly_trips_stations_ingestor/main.py
@@ -1,34 +1,44 @@
 import common.env_utils
 
-from doubled_yearly_trips_stations_ingestor import DoubledYearlyTripsStationsIngestor
-from common.rabbitmq.queue_reader import QueueReader
 from common.rabbitmq.exchange_writer import ExchangeWriter
+from common.rabbitmq.queue import Queue
+from common.processing_node.processing_node import ProcessingNode
+from common.processing_node.identity_process_input import identity_process_input
+from common.processing_node.forwarding_output_processor import ForwardingOutputProcessor
 
 
 def main():
     config = common.env_utils.read_config()
 
-    queue_bindings = common.env_utils.parse_queue_bindings(config['TRIPS_INPUT_QUEUE_BINDINGS'])
-    trips_queue_reader = QueueReader(
-        queue_name=config['TRIPS_INPUT_QUEUE_NAME'],
-        queue_bindings=queue_bindings,
+    trips_input_queue_bindings = common.env_utils.parse_queue_bindings(config['TRIPS_INPUT_QUEUE_BINDINGS'])
+    trips_input_queue = Queue(
+        hostname='rabbitmq',
+        name=config['TRIPS_INPUT_QUEUE_NAME'],
+        bindings=trips_input_queue_bindings,
         exchange_type='fanout'
     )
 
-    trips_exchange_writer = ExchangeWriter(
+    trips_output_exchange_writer = ExchangeWriter(
         exchange_name=config['TRIPS_OUTPUT_EXCHANGE_NAME'],
-        queue_name=config['TRIPS_OUTPUT_QUEUE_NAME']
+        queue_name=config['TRIPS_OUTPUT_QUEUE_NAME'],
     )
 
-    n_stations_joiners = int(config['N_BY_YEAR_TRIPS_FILTERS'])
-
-    ingestor = DoubledYearlyTripsStationsIngestor(
-        trips_queue_reader,
-        trips_exchange_writer,
-        n_stations_joiners
+    output_processor = ForwardingOutputProcessor(
+        n_output_peers=int(config['N_BY_YEAR_TRIPS_FILTERS']),
+        output_exchange_writer=trips_output_exchange_writer,
+        output_eof=common.network.constants.TRIPS_END_ALL
     )
 
-    ingestor.run()
+    processing_node = ProcessingNode(
+        process_input=identity_process_input,
+        input_eof=common.network.constants.TRIPS_END_ALL,
+        n_input_peers=1,
+        input_queue=trips_input_queue,
+        output_processor=output_processor
+    )
+
+    processing_node.run()
+
 
 
 if __name__ == "__main__":
