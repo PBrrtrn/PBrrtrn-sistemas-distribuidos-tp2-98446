79a482e8f577f1c50a7ead5ca91444949065f4f6
andrezszambrano <andreskilla2000@gmail.com>
2023-06-20 15:12:52 -0300

    trip duration

--- backend/trip_duration_running_avg/main.py
+++ backend/trip_duration_running_avg/main.py
@@ -1,10 +1,12 @@
 from trip_duration_running_avg import TripDurationRunningAvg
 import common.env_utils
-from common.rabbitmq.queue_reader import QueueReader
-
+from common.rabbitmq.queue import Queue
+from common.processing_node.identity_process_input import identity_process_input_without_header
+from common.processing_node.processing_node import ProcessingNode
+from trip_duration_storage_output_processor import TripDurationStorageOutputProcessor
 
 def main():
-    config = common.env_utils.read_config()
+    """config = common.env_utils.read_config()
 
     trips_input_queue_bindings = common.env_utils.parse_queue_bindings(config["TRIPS_INPUT_QUEUE_BINDINGS"])
     trips_input_queue_name = config["TRIPS_INPUT_QUEUE_NAME"]
@@ -16,8 +18,29 @@ def main():
     rpc_queue_reader = QueueReader(queue_name=config["RPC_QUEUE_NAME"])
 
     by_date_duration_running_avg = TripDurationRunningAvg(trips_input_queue_reader, rpc_queue_reader)
-    by_date_duration_running_avg.run()
+    by_date_duration_running_avg.run()"""
 
+    config = common.env_utils.read_config()
+
+    trips_input_queue_bindings = common.env_utils.parse_queue_bindings(config["TRIPS_INPUT_QUEUE_BINDINGS"])
+    trips_input_queue_name = config["TRIPS_INPUT_QUEUE_NAME"]
+    trips_input_queue_reader = Queue(
+        queue_name=trips_input_queue_name,
+        queue_bindings=trips_input_queue_bindings
+    )
+
+    rpc_queue_reader = Queue(queue_name=config["RPC_QUEUE_NAME"])
+    storage_output_processor = TripDurationStorageOutputProcessor(rpc_queue_reader)
+
+    processing_node = ProcessingNode(
+        process_input=identity_process_input_without_header,
+        input_eof=common.network.constants.TRIPS_END_ALL,
+        n_input_peers=1,
+        input_queue=trips_input_queue_reader,
+        output_processor=storage_output_processor
+    )
+
+    processing_node.run()
 
 if __name__ == "__main__":
     main()
