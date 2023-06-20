1f184f618aa65bf7a39cb561430452b6dfa17e85
andrezszambrano <andreskilla2000@gmail.com>
2023-06-20 13:07:18 -0300

    Montreal joiner input processor set

--- backend/montreal_stations_joiner/main.py
+++ backend/montreal_stations_joiner/main.py
@@ -1,15 +1,18 @@
-from common.rabbitmq.queue_reader import QueueReader
 from common.rabbitmq.rpc_client import RPCClient
 from common.rabbitmq.exchange_writer import ExchangeWriter
+from common.rabbitmq.queue import Queue
+from common.processing_node.processing_node import ProcessingNode
+from common.processing_node.forwarding_output_processor import ForwardingOutputProcessor
+from montreal_joiner_input_processor import MontrealJoinerInputProcessor
 
-from montreal_stations_joiner import MontrealStationsJoiner
+import common.network.constants
 
 from common.env_utils import read_config
 from common.env_utils import parse_queue_bindings
 
 
 def main():
-    config = read_config()
+    """"config = read_config()
 
     trips_input_queue_bindings = parse_queue_bindings(config['TRIPS_INPUT_QUEUE_BINDINGS'])
     trips_input_queue_reader = QueueReader(
@@ -32,8 +35,39 @@ def main():
         trips_output_exchange_writer,
         n_distance_calculators
     )
-    stations_joiner.run()
+    stations_joiner.run()"""
 
+    config = read_config()
+
+    trips_input_queue_bindings = parse_queue_bindings(config['TRIPS_INPUT_QUEUE_BINDINGS'])
+    trips_input_queue = Queue(
+        queue_bindings=trips_input_queue_bindings,
+        queue_name=config['TRIPS_INPUT_QUEUE_NAME']
+    )
+
+    stations_join_rpc_client = RPCClient(rpc_queue_name=config['STATIONS_JOIN_RPC_QUEUE_NAME'])
+
+    input_processor = MontrealJoinerInputProcessor(stations_join_rpc_client)
+
+    trips_output_exchange_writer = ExchangeWriter(
+        exchange_name=config['JOINED_TRIPS_OUTPUT_EXCHANGE_NAME'],
+        queue_name=config['JOINED_TRIPS_OUTPUT_QUEUE_NAME']
+    )
+
+    output_processor = ForwardingOutputProcessor(
+        n_output_peers=int(config['N_DISTANCE_CALCULATORS']),
+        output_exchange_writer=trips_output_exchange_writer,
+        output_eof=common.network.constants.TRIPS_END_ALL
+    )
+    processing_node = ProcessingNode(
+        process_input=input_processor.process_input,
+        input_eof=common.network.constants.TRIPS_END_ALL,
+        n_input_peers=1,
+        input_queue=trips_input_queue,
+        output_processor=output_processor
+    )
+
+    processing_node.run()
 
 if __name__ == "__main__":
     main()
---
+++ backend/montreal_stations_joiner/montreal_joiner_input_processor.py
@@ -0,0 +1,20 @@
+import pickle
+
+import common.network.constants
+import common.network.deserialize
+from common.rabbitmq.rpc_client import RPCClient
+
+
+class MontrealJoinerInputProcessor:
+    MONTREAL = 'montreal'
+
+    def __init__(self, stations_join_rpc_client: RPCClient):
+        self.stations_join_rpc_client = stations_join_rpc_client
+
+    def process_input(self, _message_type: bytes, message_body: bytes):
+        message = pickle.loads(message_body)
+        city = message[1]
+        if city == self.MONTREAL:
+            join_trips_request = common.network.constants.TRIPS_BATCH + message_body
+            raw_joined_montreal_trips = self.stations_join_rpc_client.call(join_trips_request)
+            return common.network.constants.TRIPS_BATCH + raw_joined_montreal_trips
