2ba9d5a09834508336e54da640c3331cd81b0892
andrezszambrano <andreskilla2000@gmail.com>
2023-06-23 15:19:11 -0300

    montreal station joiner working

--- backend/with_precipitations_avg_duration_trip_ingestor/main.py
+++ backend/with_precipitations_avg_duration_trip_ingestor/main.py
@@ -8,28 +8,6 @@ from common.processing_node.forwarding_output_processor import ForwardingOutputP
 from with_precipitation_input_processor import PrecipitationAvgDurationTripIngestorProcessor
 
 def main():
-    """config = common.env_utils.read_config()
-
-    trips_queue_bindings = common.env_utils.parse_queue_bindings(config['TRIPS_INPUT_QUEUE_BINDINGS'])
-    trips_queue_reader = QueueReader(
-        queue_name=config['TRIPS_INPUT_QUEUE_NAME'],
-        queue_bindings=trips_queue_bindings,
-        exchange_type='fanout'
-    )
-
-    weather_rpc_client = RPCClient(config['WEATHER_JOIN_RPC_QUEUE_NAME'])
-
-    running_avg_duration_exchange_writer = ExchangeWriter(
-        exchange_name=config['RUNNING_AVG_DURATION_EXCHANGE_NAME'],
-        queue_name=config['RUNNING_AVG_DURATION_QUEUE_NAME']
-    )
-
-    ingestor = WithPrecipitationsAvgDurationTripIngestor(
-        trips_queue_reader,
-        weather_rpc_client,
-        running_avg_duration_exchange_writer
-    )
-    ingestor.run()"""
     config = common.env_utils.read_config()
 
     trips_queue_bindings = common.env_utils.parse_queue_bindings(config['TRIPS_INPUT_QUEUE_BINDINGS'])
