c0bfdd623dab48c31ac1d811891294dd56722a0a
andrezszambrano <andreskilla2000@gmail.com>
2023-06-23 16:40:23 -0300

    Weather manager refactored

--- backend/weather_manager/main.py
+++ backend/weather_manager/main.py
@@ -2,9 +2,10 @@ import os
 from configparser import ConfigParser
 
 import common.env_utils
-from weather_manager import WeatherManager
-from common.rabbitmq.queue_reader import QueueReader
-
+from common.rabbitmq.queue import Queue
+from weather_manager_output_processor import WeatherManagerOutputProcessor
+from common.processing_node.identity_process_input import identity_process_input
+from common.processing_node.processing_node import ProcessingNode
 
 def read_config():
     config = ConfigParser(os.environ)
@@ -17,15 +18,26 @@ def main():
     config = read_config()
 
     input_queue_bindings = common.env_utils.parse_queue_bindings(config['FILTERED_WEATHER_QUEUE_BINDINGS'])
-    input_queue_reader = QueueReader(
-        queue_name=config['FILTERED_WEATHER_QUEUE_NAME'],
-        queue_bindings=input_queue_bindings
+    weather_queue = Queue(
+        hostname='rabbitmq',
+        name=config['FILTERED_WEATHER_QUEUE_NAME'],
+        bindings=input_queue_bindings
     )
 
-    n_weather_filters = config['N_WEATHER_FILTERS']
-    rpc_queue_reader = QueueReader(queue_name=config['WEATHER_RPC_QUEUE_NAME'])
-    weather_manager = WeatherManager(input_queue_reader, int(n_weather_filters), rpc_queue_reader)
-    weather_manager.run()
+    rpc_queue = Queue(
+        hostname='rabbitmq',
+        name=config['WEATHER_RPC_QUEUE_NAME']
+    )
+
+    weather_manager_output_processor = WeatherManagerOutputProcessor(rpc_queue=rpc_queue)
+    processing_node = ProcessingNode(
+        process_input=identity_process_input,
+        input_eof=common.network.constants.WEATHER_END_ALL,
+        n_input_peers=int(config['N_WEATHER_FILTERS']),
+        input_queue=weather_queue,
+        output_processor=weather_manager_output_processor
+    )
+    processing_node.run()
 
 
 if __name__ == "__main__":
