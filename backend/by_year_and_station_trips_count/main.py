c5db2fe595fd2659b17c7e16653dfe79e2c978af
andrezszambrano <andreskilla2000@gmail.com>
2023-06-23 15:40:15 -0300

    counting node working

--- backend/by_year_and_station_trips_count/main.py
+++ backend/by_year_and_station_trips_count/main.py
@@ -9,35 +9,19 @@ import common.network.constants
 
 
 def main():
-    """config = common.env_utils.read_config()
-
-    filtered_trips_input_queue_bindings = common.env_utils.parse_queue_bindings(config['FILTERED_TRIPS_QUEUE_BINDINGS'])
-    filtered_trips_input_queue_reader = QueueReader(
-        queue_name=config['FILTERED_TRIPS_QUEUE_NAME'],
-        queue_bindings=filtered_trips_input_queue_bindings
-    )
-
-    requests_queue_reader = QueueReader(queue_name=config['DOUBLED_YEARLY_TRIPS_STATIONS_RPC_QUEUE_NAME'])
-    stations_rpc_client = RPCClient(rpc_queue_name=config['STATIONS_RPC_QUEUE_NAME'])
-    n_by_year_trips_filters = int(config['N_BY_YEAR_TRIPS_FILTERS'])
-
-    by_year_and_station_trips_count = ByYearAndStationTripsCount(
-        filtered_trips_input_queue_reader,
-        requests_queue_reader,
-        stations_rpc_client,
-        n_by_year_trips_filters
-    )
-
-    by_year_and_station_trips_count.run()"""
     config = common.env_utils.read_config()
 
     filtered_trips_input_queue_bindings = common.env_utils.parse_queue_bindings(config['FILTERED_TRIPS_QUEUE_BINDINGS'])
     filtered_trips_input_queue_reader = Queue(
-        queue_name=config['FILTERED_TRIPS_QUEUE_NAME'],
-        queue_bindings=filtered_trips_input_queue_bindings
+        hostname='rabbitmq',
+        name=config['FILTERED_TRIPS_QUEUE_NAME'],
+        bindings=filtered_trips_input_queue_bindings
     )
 
-    requests_queue_reader = Queue(queue_name=config['DOUBLED_YEARLY_TRIPS_STATIONS_RPC_QUEUE_NAME'])
+    requests_queue_reader = Queue(
+        hostname='rabbitmq',
+        name=config['DOUBLED_YEARLY_TRIPS_STATIONS_RPC_QUEUE_NAME']
+    )
     stations_rpc_client = RPCClient(rpc_queue_name=config['STATIONS_RPC_QUEUE_NAME'])
     storage_output_processor = StorageOutputProcessor(requests_queue_reader, stations_rpc_client)
 
