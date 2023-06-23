a30a745a30c1adcd5c7684fbebf26029b10409a7
andrezszambrano <andreskilla2000@gmail.com>
2023-06-23 16:19:14 -0300

    station manager refactored to new queue

--- backend/stations_manager/stations_manager_output_processor.py
+++ backend/stations_manager/stations_manager_output_processor.py
@@ -1,12 +1,12 @@
 import pickle
 
-from common.rabbitmq.queue_reader import QueueReader
+from common.rabbitmq.queue import Queue
 import common.network.deserialize
 
 
 class StationsManagerOutputProcessor:
-    def __init__(self, rpc_queue_reader: QueueReader, n_montreal_stations_joiners: int):
-        self.rpc_queue_reader = rpc_queue_reader
+    def __init__(self, rpc_queue: Queue, n_montreal_stations_joiners: int):
+        self.rpc_queue = rpc_queue
         self.n_montreal_stations_joiners = n_montreal_stations_joiners
 
         self.storage = {}
@@ -27,22 +27,21 @@ class StationsManagerOutputProcessor:
             self.storage[city][station.code] = station
 
     def finish_processing(self):
-        self.rpc_queue_reader.consume(callback=self.respond_rpc, auto_ack=False)
+        for (method, properties, message) in self.rpc_queue.read_with_props():
+            message_type = message[:common.network.constants.HEADER_TYPE_LEN]
+            raw_message = message[common.network.constants.HEADER_TYPE_LEN:]
+            if message_type == common.network.constants.TRIPS_BATCH:
+                self.join_trips(method, properties, raw_message)
+            elif message_type == common.network.constants.STATIONS_BATCH:
+                self.join_stations(method, properties, raw_message)
+            elif message_type in [common.network.constants.TRIPS_END_ALL, common.network.constants.STATIONS_END]:
+                self.process_shutdown_signal(method, properties)
+            else:
+                print(f"ERROR - Unknown message header (got {message_type})")
 
-    def respond_rpc(self, _channel, method, properties, body):
-        message_type = body[:common.network.constants.HEADER_TYPE_LEN]
-        raw_message = body[common.network.constants.HEADER_TYPE_LEN:]
-        if message_type == common.network.constants.TRIPS_BATCH:
-            self.join_trips(method, properties, raw_message)
-        elif message_type == common.network.constants.STATIONS_BATCH:
-            self.join_stations(method, properties, raw_message)
-        elif message_type in [common.network.constants.TRIPS_END_ALL, common.network.constants.STATIONS_END]:
-            self.process_shutdown_signal(method, properties)
-        else:
-            print(f"ERROR - Unknown message header (got {message_type})")
 
     def process_shutdown_signal(self, method, properties):
-        self.rpc_queue_reader.respond(
+        self.rpc_queue.respond(
             message=b'',
             to=properties.reply_to,
             correlation_id=properties.correlation_id,
@@ -50,7 +49,7 @@ class StationsManagerOutputProcessor:
         )
         self.eofs_received += 1
         if self.eofs_received >= 1 + self.n_montreal_stations_joiners:
-            self.rpc_queue_reader.shutdown()
+            self.rpc_queue.close()
 
     def join_stations(self, method, properties, raw_message):
         station_codes, city = pickle.loads(raw_message)
@@ -59,7 +58,7 @@ class StationsManagerOutputProcessor:
             station = self.storage[city][station_code]
             response.append(station.name)
         serialized_response = pickle.dumps(response)
-        self.rpc_queue_reader.respond(
+        self.rpc_queue.respond(
             message=serialized_response,
             to=properties.reply_to,
             correlation_id=properties.correlation_id,
@@ -76,7 +75,7 @@ class StationsManagerOutputProcessor:
                 end_station = self.storage[city][trip.end_station_code]
                 response.append((trip, start_station, end_station))
         serialized_response = pickle.dumps(response)
-        self.rpc_queue_reader.respond(
+        self.rpc_queue.respond(
             message=serialized_response,
             to=properties.reply_to,
             correlation_id=properties.correlation_id,
