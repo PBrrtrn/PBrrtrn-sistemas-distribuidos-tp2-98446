21038fcdbc3c28a9347510f5034bd732bb97066c
andrezszambrano <andreskilla2000@gmail.com>
2023-06-23 00:39:14 -0300

    Deleted old code

--- backend/weather_filter/main.py
+++ backend/weather_filter/main.py
@@ -20,22 +20,6 @@ def read_config():
 
 
 def main():
-    """config = read_config()
-
-    queue_bindings = common.env_utils.parse_queue_bindings(config['UNFILTERED_WEATHER_QUEUE_BINDINGS'])
-    input_queue_reader = QueueReader(
-        queue_name=config["UNFILTERED_WEATHER_QUEUE_NAME"],
-        queue_bindings=queue_bindings
-    )
-
-    output_exchange_writer = ExchangeWriter(
-        exchange_name=config["FILTERED_WEATHER_EXCHANGE_NAME"],
-        queue_name=config["FILTERED_WEATHER_QUEUE_NAME"]
-    )
-
-    weather_filter = WeatherFilter(input_queue_reader, output_exchange_writer)
-    weather_filter.run()"""
-
     config = common.env_utils.read_config()
 
     queue_bindings = common.env_utils.parse_queue_bindings(config['UNFILTERED_WEATHER_QUEUE_BINDINGS'])
