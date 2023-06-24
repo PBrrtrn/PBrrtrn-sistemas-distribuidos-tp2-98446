456e88e30dbfa96a309f1c641193a68160f9c392
andrezszambrano <andreskilla2000@gmail.com>
2023-06-24 18:56:06 -0300

    StorageHandler refactored

--- backend/by_year_and_station_trips_count/station_counter_storage_handler.py
+++ backend/by_year_and_station_trips_count/station_counter_storage_handler.py
@@ -3,18 +3,12 @@ import pickle
 
 from common.processing_node.storage_handler import StorageHandler
 
-LOG_FILE_PATH = "path"
-STORAGE_FILE_PATH = "storage_path"
-AUX_STORAGE_FILE_PATH = "storage_path"
-COMMIT_CHAR = "~"
-
 
 class StationCounterStorageHandler(StorageHandler):
     def __init__(self):
-        super(StorageHandler, self).__init__()
-        self.storage = {}
+        super().__init__()
 
-    def prepare(self, message: bytes):
+    def _generate_log_map(self, message: bytes):
         trips, city = pickle.loads(message)
         to_log = {}
         for trip in trips:
@@ -31,11 +25,7 @@ class StationCounterStorageHandler(StorageHandler):
                     '2017': old_value_2017
                 }
             to_log[city][trip.start_station_code][trip_year] += 1
-
-        # update memory map
-        # flush log to file (operations can be interchangeably done, does not matter the order)
-        self.__update_memory_map_with_logs(to_log)
-        self.__write_log_line(to_log)
+        return to_log
 
     def __obtain_old_value_of_station_counter_in_years(self, city, start_station_code):
         if city not in self.storage:
@@ -47,18 +37,10 @@ class StationCounterStorageHandler(StorageHandler):
             old_value_2017 = self.storage[city][start_station_code]['2017']
             return old_value_2016, old_value_2017
 
-    def __write_log_line(self, to_log):
-        pass
-        # with open(LOG_FILE_PATH, "w") as file:
-        #   json.dump(to_log, file) #Archivo ya abierto + flush
-
-    def commit(self):
-        pass
-
     def __update_changes_in_disk(self):
         pass
 
-    def __update_memory_map_with_logs(self, log_map):
+    def _update_memory_map_with_logs(self, log_map):
         for city in log_map:
             if city not in self.storage:
                 self.storage[city] = {}
@@ -69,11 +51,3 @@ class StationCounterStorageHandler(StorageHandler):
                     new_value = log_map[city][start_station_code][year]
                     self.storage[city][start_station_code][year] = new_value
 
-    def get_storage(self):
-        return self.storage
-
-    def prepare_delete(self):
-        pass
-
-    def commit_delete(self):
-        pass
--- backend/stations_distance_running_avg/station_distance_storage_handler.py
+++ backend/stations_distance_running_avg/station_distance_storage_handler.py
@@ -3,18 +3,12 @@ import pickle
 
 from common.processing_node.storage_handler import StorageHandler
 
-LOG_FILE_PATH = "path"
-STORAGE_FILE_PATH = "storage_path"
-AUX_STORAGE_FILE_PATH = "storage_path"
-COMMIT_CHAR = "~"
-
 
 class StationDistanceStorageHandler(StorageHandler):
     def __init__(self):
-        super(StorageHandler, self).__init__()
-        self.storage = {}
+        super().__init__()
 
-    def prepare(self, message: bytes):
+    def _generate_log_map(self, message: bytes):
         joined_trips_with_distance = pickle.loads(message)
 
         for end_station_name, distance in joined_trips_with_distance:
@@ -23,28 +17,10 @@ class StationDistanceStorageHandler(StorageHandler):
                 self.storage[end_station_name]["n_trips"] += 1
             else:
                 self.storage[end_station_name] = {"total_distance": distance, "n_trips": 1}
-        #self.__update_memory_map_with_logs(to_log)
-        #self.__write_log_line(to_log)
 
-    def __write_log_line(self, to_log):
-        pass
-        # with open(LOG_FILE_PATH, "w") as file:
-        #   json.dump(to_log, file) #Archivo ya abierto + flush
-
-    def commit(self):
+    def _update_memory_map_with_logs(self, log_map):
         pass
 
     def __update_changes_in_disk(self):
         pass
 
-    def __update_memory_map_with_logs(self, log_map):
-        pass
-
-    def get_storage(self):
-        return self.storage
-
-    def prepare_delete(self):
-        pass
-
-    def commit_delete(self):
-        pass
