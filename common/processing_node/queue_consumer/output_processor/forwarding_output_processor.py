ed7d12102eda8d126a790815ab419dee5f80c915
andrezszambrano <andreskilla2000@gmail.com>
2023-06-29 18:08:01 -0300

    Forwarding output processor has a file for each client

--- common/processing_node/queue_consumer/output_processor/forwarding_output_processor.py
+++ common/processing_node/queue_consumer/output_processor/forwarding_output_processor.py
@@ -14,10 +14,8 @@ class ForwardingOutputProcessor:
         self.output_eof = output_eof
         self.optional_rpc_eof = optional_rpc_eof
         self.forward_with_routing_key = forward_with_routing_key
-        self.forwarding_state_storage_handler = ForwardingStateStorageHandler(
-            storage_directory=DIR,
-            filename=FILENAME
-        )
+        self.clients_storage_handler_dict = {} #Cargar de un archivo en donde estén los ids de los clientes
+        # {"id_cliente": ForwardingStateStorageHandler}
 
     def process_output(self, channel, message: bytes, method, _properties, client_id):
         if message is None:
@@ -26,24 +24,26 @@ class ForwardingOutputProcessor:
 
         # if self.storage["id_last_message_forwarded"] == message.id: # Message id hay q cargarlo
         #    channel.basic_ack(delivery_tag=method.delivery_tag)
-
-        self.forwarding_state_storage_handler.prepare_last_message_id_increment()
+        if client_id not in self.clients_storage_handler_dict:
+            self.clients_storage_handler_dict[client_id] = \
+                ForwardingStateStorageHandler(storage_directory=DIR, filename=FILENAME, client_id=client_id)
+        self.clients_storage_handler_dict[client_id].prepare_last_message_id_increment()
         self._forward(self.output_exchange_writer, message, client_id)
-        self.forwarding_state_storage_handler.commit()
+        self.clients_storage_handler_dict[client_id].commit()
         channel.basic_ack(delivery_tag=method.delivery_tag)
 
     def finish_processing(self, client_id):
-        storage = self.forwarding_state_storage_handler.get_storage()
+        storage = self.clients_storage_handler_dict[client_id].get_storage()
         if not storage.get("rpc_eof_sent", False) and self.optional_rpc_eof is not None:
-            self.forwarding_state_storage_handler.prepare_set_rpc_eof_as_sent()
+            self.clients_storage_handler_dict[client_id].prepare_set_rpc_eof_as_sent()
             self.optional_rpc_eof.write_eof(self.output_eof, routing_key_suffix=client_id)
-            self.forwarding_state_storage_handler.commit()
+            self.clients_storage_handler_dict[client_id].commit()
         remaining_eofs = self.n_output_peers - storage.get("eofs_sent", 0)
         for i in range(remaining_eofs):
-            self.forwarding_state_storage_handler.prepare_eofs_sent_increment()
+            self.clients_storage_handler_dict[client_id].prepare_eofs_sent_increment()
             self._forward_eof(self.output_exchange_writer, self.output_eof, client_id)
             # self.output_exchange_writer.write(self.output_eof, routing_key_suffix='1')
-            self.forwarding_state_storage_handler.commit()
+            self.clients_storage_handler_dict[client_id].commit()
 
     def _forward(self, exchange_writer, message, client_id):
         if self.forward_with_routing_key:
