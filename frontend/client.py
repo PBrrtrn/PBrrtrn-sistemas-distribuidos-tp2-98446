41abcee2dedcd3278f6477b80bfedea27bf835ba
andrezszambrano <andreskilla2000@gmail.com>
2023-06-30 18:45:47 -0300

    Working with two clients

--- frontend/client.py
+++ frontend/client.py
@@ -3,6 +3,7 @@ import pickle
 import socket
 import datetime
 from time import sleep
+import random
 
 import common.network.serialize
 import common.network.constants
@@ -20,7 +21,7 @@ class Client:
         self.config = config
 
     def run(self):
-        sleep(10)
+        sleep(10 + random.randint(0,9))
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         retries = 0
         while retries < 5:
