fb64841f48b993fbf65146119433ffcc2009ac54
andrezszambrano <andreskilla2000@gmail.com>
2023-06-30 11:45:07 -0300

    Testing but not working

--- backend/data_ingestion_server/data_ingestion_server.py
+++ backend/data_ingestion_server/data_ingestion_server.py
@@ -1,13 +1,12 @@
 from multiprocessing import Process
+from typing import Callable
 
 from common.network.socket_wrapper import SocketWrapper
-
 
 import socket
 
 from common.rabbitmq.exchange_writer import ExchangeWriter
 from common.rabbitmq.fanout_exchange_writer import FanoutExchangeWriter
-from common.rabbitmq.rpc_client import RPCClient
 import common.network.constants
 
 from client_data_ingestor import ClientDataIngestor
@@ -16,26 +15,17 @@ from client_data_ingestor import ClientDataIngestor
 class DataIngestionServer:
     def __init__(self, port,
                  listen_backlog,
-                 stations_exchange_writer: ExchangeWriter,
-                 weather_exchange_writer: ExchangeWriter,
+                 exchange_writers_factory: Callable,
+                 rpc_clients_factory: Callable,
                  n_weather_filters: int,
-                 trips_exchange_writer: FanoutExchangeWriter,
-                 new_clients_exchange_writer: ExchangeWriter,
-                 montreal_stations_over_6km_avg_trip_distance_queue_name: str,
-                 with_precipitations_avg_trip_duration_queue_name: str,
-                 doubled_yearly_trips_stations_queue_name: str):
+                 config):
         self.port = port
         self.listen_backlog = listen_backlog
-        self.stations_exchange_writer = stations_exchange_writer
-        self.weather_exchange_writer = weather_exchange_writer
+        self.exchange_writers_factory = exchange_writers_factory
+        self.rpc_clients_factory = rpc_clients_factory
         self.n_weather_filters = n_weather_filters
-        self.trips_exchange_writer = trips_exchange_writer
-        self.new_clients_exchange_writer = new_clients_exchange_writer
-        self.montreal_stations_over_6km_avg_trip_distance_queue_name =\
-            montreal_stations_over_6km_avg_trip_distance_queue_name
-        self.with_precipitations_avg_trip_duration_queue_name = with_precipitations_avg_trip_duration_queue_name
-        self.doubled_yearly_trips_stations_queue_name = doubled_yearly_trips_stations_queue_name
         self.finished = False
+        self.config = config
         self.clients_id = []
 
     def run(self):
@@ -47,19 +37,17 @@ class DataIngestionServer:
             client_socket, _ = server_socket.accept()
             wrapped_socket = SocketWrapper(client_socket)
             client_id = str(len(self.clients_id) + 1).zfill(common.network.constants.CLIENT_ID_LEN)
-            montreal_stations_over_6km_avg_trip_distance_rpc = RPCClient(
-                f"{self.montreal_stations_over_6km_avg_trip_distance_queue_name}{client_id}"
-            )
-            with_precipitations_avg_trip_duration_rpc = RPCClient(
-                f"{self.with_precipitations_avg_trip_duration_queue_name}{client_id}"
-            )
-            doubled_yearly_trips_stations_rpc = RPCClient(
-                f"{self.doubled_yearly_trips_stations_queue_name}{client_id}"
-            )
-            client_ingestor = ClientDataIngestor(wrapped_socket, client_id, self.stations_exchange_writer,
-                                                 self.weather_exchange_writer, self.n_weather_filters,
-                                                 self.trips_exchange_writer,
-                                                 self.new_clients_exchange_writer,
+            stations_exchange_writer, weather_exchange_writer, trips_exchange_writer, \
+            new_clients_exchange_writer = self.exchange_writers_factory(self.config)
+            print(stations_exchange_writer, weather_exchange_writer, trips_exchange_writer, new_clients_exchange_writer)
+            montreal_stations_over_6km_avg_trip_distance_rpc, with_precipitations_avg_trip_duration_rpc, \
+            doubled_yearly_trips_stations_rpc = self.rpc_clients_factory(self.config, client_id)
+            print(montreal_stations_over_6km_avg_trip_distance_rpc, with_precipitations_avg_trip_duration_rpc, doubled_yearly_trips_stations_rpc)
+
+            client_ingestor = ClientDataIngestor(wrapped_socket, client_id, stations_exchange_writer,
+                                                 weather_exchange_writer, self.n_weather_filters,
+                                                 trips_exchange_writer,
+                                                 new_clients_exchange_writer,
                                                  montreal_stations_over_6km_avg_trip_distance_rpc,
                                                  with_precipitations_avg_trip_duration_rpc,
                                                  doubled_yearly_trips_stations_rpc)
--- backend/data_ingestion_server/main.py
+++ backend/data_ingestion_server/main.py
@@ -3,6 +3,7 @@ from configparser import ConfigParser
 
 from common.rabbitmq.exchange_writer import ExchangeWriter
 from common.rabbitmq.fanout_exchange_writer import FanoutExchangeWriter
+from common.rabbitmq.rpc_client import RPCClient
 from data_ingestion_server import DataIngestionServer
 
 
@@ -13,9 +14,7 @@ def read_config():
     return config["DEFAULT"]
 
 
-def main():
-    config = read_config()
-
+def exchange_writers_factory(config):
     stations_exchange_writer = ExchangeWriter(
         hostname=config["RABBITMQ_HOSTNAME"],
         exchange_name=config["STATIONS_EXCHANGE_NAME"],
@@ -36,17 +35,34 @@ def main():
         queue_name=config['NEW_CLIENT_QUEUE_NAME'],
         exchange_type='fanout')
 
+    return stations_exchange_writer, weather_exchange_writer, trips_exchange_writer, new_clients_exchange_writer
+
+
+def rpc_clients_factory(config, client_id):
+    montreal_stations_over_6km_avg_trip_distance_rpc = RPCClient(
+        f"{config['MONTREAL_STATIONS_OVER_6KM_AVG_TRIP_DISTANCE_RPC_QUEUE_NAME']}{client_id}"
+    )
+    with_precipitations_avg_trip_duration_rpc = RPCClient(
+        f"{config['WITH_PRECIPITATIONS_AVG_TRIP_DURATION_RPC_QUEUE_NAME']}{client_id}"
+    )
+    doubled_yearly_trips_stations_rpc = RPCClient(
+        f"{config['DOUBLED_YEARLY_TRIPS_STATIONS_RPC_QUEUE_NAME']}{client_id}"
+    )
+
+    return montreal_stations_over_6km_avg_trip_distance_rpc, with_precipitations_avg_trip_duration_rpc, \
+           doubled_yearly_trips_stations_rpc
+
+
+def main():
+    config = read_config()
+
     server = DataIngestionServer(
         int(config["PORT"]),
         int(config["LISTEN_BACKLOG"]),
-        stations_exchange_writer,
-        weather_exchange_writer,
+        exchange_writers_factory,
+        rpc_clients_factory,
         int(config['N_WEATHER_FILTERS']),
-        trips_exchange_writer,
-        new_clients_exchange_writer,
-        config['MONTREAL_STATIONS_OVER_6KM_AVG_TRIP_DISTANCE_RPC_QUEUE_NAME'],
-        config['WITH_PRECIPITATIONS_AVG_TRIP_DURATION_RPC_QUEUE_NAME'],
-        config['DOUBLED_YEARLY_TRIPS_STATIONS_RPC_QUEUE_NAME']
+        config
     )
     server.run()
 
